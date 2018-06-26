122104f39a065acf9f407019367ce14caf3f7f95
Bertrand Steinmetz <bstenm@hotmail.com>
2018-06-26 15:10:33 -0500

    maintenance

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,11 @@ var CorrToken = artifacts.require("./CorrToken.sol");
 var CorrTokenSale = artifacts.require("./CorrTokenSale.sol");
 
 module.exports = function (deployer) {
-      deployer.deploy(Voting, config.token.supply, web3.toWei(config.token.price, 'ether'), config.candidates);
-      deployer.deploy(CorrToken, 1000000).then(() => {
-            return deployer.deploy(CorrTokenSale, CorrToken.address, 1000000000);
+      ///////////////////////////////////////////
+      // var tokenPrice = 1000000000;
+      var tokenPrice = web3.toWei(config.token.price, 'ether');
+      deployer.deploy(Voting, config.token.supply, tokenPrice, config.candidates);
+      deployer.deploy(CorrToken, config.token.supply).then(() => {
+            return deployer.deploy(CorrTokenSale, CorrToken.address, tokenPrice);
       });
 };
