81f530c1d54c28063d87af7f77fb4de7b74e1843
Bertrand Steinmetz <bstenm@hotmail.com>
2018-10-20 11:08:52 -0500

    display address rather than name

--- src/components/UserAccount/UserAccount.js
+++ src/components/UserAccount/UserAccount.js
@@ -8,7 +8,7 @@ import {Glyphicon} from 'react-bootstrap';
 export const UserAccount = ({account: {tokens, address, votingRecord}}) => (
       <div className="UserAccount" >
             <div className="userInfo">
-                  <div><Glyphicon glyph="user"/>{address}</div>
+                  <div><Glyphicon glyph="user"/><span  title={address}>{address.slice(0, 15)}...</span></div>
                   <div><Glyphicon glyph="usd"/>{tokens} tokens left</div>
             </div>
             {! isEmpty(votingRecord) &&
