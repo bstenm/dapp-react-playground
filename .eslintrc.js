f038335af25399b29a9ee53cd61b448ac67a87d0
Bertrand Steinmetz <bstenm@hotmail.com>
2018-10-28 09:58:50 -0500

    adding prettier to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,15 @@
+// [REF]: https://medium.freecodecamp.org/these-tools-will-help-you-write-clean-code-da4b5401f68e
 module.exports = {
 	env: {
 		es6: true,
 		browser: true,
 		node: true,
 	},
-	extends: ['airbnb', 'plugin:jest/recommended', 'jest-enzyme'],
+	extends: [
+            'airbnb',
+            'plugin:jest/recommended',
+            'jest-enzyme'
+      ],
 	plugins: [
 		'babel',
 		'import',
@@ -22,5 +27,6 @@ module.exports = {
 	},
 	rules: {
             "indent": ["error", 6],
+            "prettier/prettier": "error",
 	}
 };
