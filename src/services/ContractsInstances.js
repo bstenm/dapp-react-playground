9f1875a373c2ab9eb09507ebbe4e62ae9464f4b9
Bertrand Steinmetz <bstenm@hotmail.com>
2018-10-31 11:48:18 -0500

    cleaning

--- src/services/ContractsInstances.js
+++ src/services/ContractsInstances.js
@@ -4,7 +4,6 @@ import usersArtifacts from '../contracts/Users.json';
 import tokenArtifacts from '../contracts/CorrToken.json';
 import tokenSaleArtifacts from '../contracts/CorrTokenSale.json';
 import candidatesArtifacts from '../contracts/Candidates.json';
-import userActionsArtifacts from '../contracts/UserActions.json';
 
 const UsersContract = contract(usersArtifacts);
 const TokenContract = contract(tokenArtifacts);
@@ -15,10 +14,8 @@ UsersContract.setProvider(web3.currentProvider);
 TokenContract.setProvider(web3.currentProvider);
 TokenSaleContract.setProvider(web3.currentProvider);
 CandidatesContract.setProvider(web3.currentProvider);
-UserActionsContract.setProvider(web3.currentProvider);
 
 export const Users = UsersContract;
 export const Token = TokenContract;
 export const TokenSale = TokenSaleContract;
 export const Candidates = CandidatesContract;
-export const UserActions = UserActionsContract;
